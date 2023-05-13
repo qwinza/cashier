17718b9225d344f0bf8cb9be97a4d6cbfd7cfdba
estradax <azharumuhammad@gmail.com>
2023-05-13 17:59:03 +0700

    feat: make login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('title', 'Login')
+
+@section('content')
+    <div class="full-size-container">
+        <div class="login-container">
+            <div class="logo-container">
+                <img src="{{ asset('logo.png') }}" alt="App logo">
+            </div>
+            <form class="login-form" action="">
+                <h1>Login</h1>
+                <div class="field-container">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name">
+                </div>
+                <div class="field-container">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password">
+                </div>
+                <button>Log in</button>
+                <a href="#">Forgot password</a>
+            </form>
+        </div>
+    </div>
+@endsection
